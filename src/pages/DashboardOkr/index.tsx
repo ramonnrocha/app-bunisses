9c4b2a4b73ccc03ad9b694f4ff1e700d48e2a982
Ramonn <ramonnsantana@gmail.com>
2022-11-14 17:21:07 -0300

    .

---
+++ src/pages/DashboardOkr/index.tsx
@@ -0,0 +1,41 @@
+import { Header } from '../../components/Header'
+import { NavBar } from '../../components/NavBar'
+import {
+  DashboardContainer,
+  DashInfo,
+  DivInfoStats,
+  StatsContainer,
+} from './styles'
+
+export function DashboardOkr() {
+  return (
+    <>
+      <Header></Header>
+      <NavBar></NavBar>
+
+      <DashboardContainer>
+        <DashInfo>
+          <h2>DashBoard</h2>
+          <StatsContainer>
+            <DivInfoStats>
+              <h2>Total</h2>
+              <strong>12</strong>
+            </DivInfoStats>
+            <DivInfoStats>
+              <h2>Em andamento</h2>
+              <strong>10</strong>
+            </DivInfoStats>
+            <DivInfoStats>
+              <h2>Atenção</h2>
+              <strong>4</strong>
+            </DivInfoStats>
+            <DivInfoStats>
+              <h2>Atrasados</h2>
+              <strong>2</strong>
+            </DivInfoStats>
+          </StatsContainer>
+        </DashInfo>
+      </DashboardContainer>
+    </>
+  )
+}
---
+++ src/pages/DashboardOkr/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+
+export const DashboardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 15rem;
+  margin-right: 3rem;
+  width: auto;
+`
+
+export const DashInfo = styled.div`
+  width: 100%;
+  height: min(70rem);
+  border: 1px solid ${(props) => props.theme['blue-black']};
+  border-radius: 8px;
+  margin: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  color: ${(props) => props.theme['blue-black']};
+
+  h2 {
+    margin-top: 2rem;
+    font-size: 3.2rem;
+  }
+`
+
+export const StatsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  margin-top: 5rem;
+  align-self: center;
+  justify-content: space-between;
+  flex-direction: row;
+  gap: 4rem;
+  margin: 4rem;
+`
+export const DivInfoStats = styled.div`
+  width: 50rem;
+  margin-inline: 3rem;
+  height: 25rem;
+  border: 3px solid ${(props) => props.theme['grey-hover']};
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  gap: 6rem;
+  color: ${(props) => props.theme['blue-black']};
+
+  strong {
+    font-size: 4rem;
+  }
+`
